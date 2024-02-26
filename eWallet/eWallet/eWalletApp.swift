96923047f57c78924d30017ccc814daec19ac603
Habibur Rahman <habibur.rahman@dreamonline.co.jp>
2024-02-26 19:38:42 +0600

    add Project

---
+++ eWallet/eWallet/eWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  eWalletApp.swift
+//  eWallet
+//
+//  Created by Habibur Rahman on 26/2/24.
+//
+
+import SwiftUI
+
+@main
+struct eWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
