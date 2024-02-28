9847cb258006bba19f4ed55d6c3aa9656d6a60c3
Oviebd <oviecse48@gmail.com>
2024-02-28 22:09:09 +0600

    add transactions

--- eWallet/eWallet/View/Home/HomeView.swift
+++ eWallet/eWallet/View/Home/HomeView.swift
@@ -8,8 +8,47 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State var showMe = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    
+        ZStack(alignment: .top) {
+        
+            VStack{
+                
+                Button{
+                    showMe.toggle()
+                }label: {
+                  Text("Click me")
+                }
+                
+                AccountsListView()
+                Spacer()
+            }
+          
+            
+            
+            if showMe {
+                ZStack{
+                    AddAccountFormView()
+                        .frame(height: 200)
+                        .transition(AnyTransition.scale.animation(.easeInOut))
+                       // .transition(.move(edge: .bottom))
+                       // .animation(.easeInOut)
+                }
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                .transition(AnyTransition.scale.animation(.easeInOut))
+                .background(Color.black.opacity(0.8))
+                
+               
+            }
+            
+            
+        }.background(Color.white)
+       
+       
+       // Spacer()
+       
     }
 }
 
