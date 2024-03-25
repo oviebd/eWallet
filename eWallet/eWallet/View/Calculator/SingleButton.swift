66d9f95d401cfb478dbd853f83a30e1893075a90
Oviebd <oviecse48@gmail.com>
2024-03-25 06:09:15 +0600

    add calculator view

---
+++ eWallet/eWallet/View/Calculator/SingleButton.swift
@@ -0,0 +1,18 @@
+//
+//  SingleView.swift
+//  eWallet
+//
+//  Created by Habibur Rahman on 25/3/24.
+//
+
+import SwiftUI
+
+struct SingleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SingleView()
+}
