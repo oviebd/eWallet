b30f6533ad54a441ed388a76e8ddda1df65b5f5c
Habibur Rahman <habibur.rahman@periscopelabs.io>
2024-08-31 22:51:27 +0600

    add dateWise Record Item View

---
+++ eWallet/eWallet/View/Record List View/All Records View/DateWiseRecordListItem.swift
@@ -0,0 +1,30 @@
+//
+//  DateWiseRecordListItem.swift
+//  eWallet
+//
+//  Created by Habibur Rahman on 31/8/24.
+//
+
+import SwiftUI
+
+struct DateWiseRecordListItem: View {
+    
+//    let date : Date
+//    let dataList :
+    
+    var body: some View {
+        
+        VStack(alignment: .leading){
+            Text("27 August")
+            
+        }
+    }
+    
+//    func getDateTitle() -> String {
+//        return dataByDateDic.
+//    }
+}
+
+//#Preview {
+//    DateWiseRecordListItem(dataByDateDic: [.now.removeTime() : DummyDataUtils.todayRecordList])
+//}
